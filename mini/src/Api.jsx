5663b74fea21c64a8b1ab561d2501f2a0f276cb3
Narshima <thatipamulanarashima321@gmail.com>
2025-12-11 23:53:00 -0800

    full-stock commit

--- mini/src/Api.jsx
+++ mini/src/Api.jsx
@@ -1,23 +1,5 @@
 
 
-// const API_URI = "http://localhost:4000/"
-
-
-// export const getVersions = async ()=> {
-//     const res = await fetch(`${API_URI}/varsions`);
-//     return res.json()
-// }
-
-// export const saveVersions = async (prevText , newText)=> {
-//     const res = await fetch(`${API_URI}/save-Version`, {
-//         method: "POST",
-//         headers: {"content-Type" : "application/json"},
-//         body: JSON.stringify({prevText, newText})
-//     });
-//     return res.json();
-   
-// }
-
 const API_BASE = "http://localhost:4000"; 
 // If deployed, replace with your backend URL
 
